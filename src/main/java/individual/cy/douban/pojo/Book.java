d0cb6d560dda4841f89d8748f193eedcb05a39b5
mystic_cg <215104920@qq.com>
2017-12-21 08:58:49 +0800

    init project

---
+++ src/main/java/individual/cy/douban/pojo/Book.java
@@ -0,0 +1,13 @@
+package individual.cy.douban.pojo;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: mystic
+ * @date: 2017/12/21 8:54
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+public class Book {
+}
---
+++ src/main/java/individual/cy/douban/web/Spider.java
@@ -0,0 +1,13 @@
+package individual.cy.douban.web;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: mystic
+ * @date: 2017/12/21 8:33
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+public class Spider {
+}
